f99eb5bc95cced060abf3eb088c68e66c469ee7c
igorgoldobin <IgorGoldobin@ssw.com.au>
2024-01-31 11:43:13 +1000

    feat: removed unused sections

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
-import { useLocation } from 'react-router-dom';
 import { Heading } from './Heading';
 import { routesEnum } from '../Routes';
 import { Link } from './Link';
-
-import { Button } from './Button';
-
-const Rhino = styled.span`
-  font-size: 20px;
-`;
 
 const RainbowBackground = styled.div`
   min-width: 100%;
@@ -40,30 +33,7 @@ const FooterStyles = styled.div`
   }
 `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const ButtonLink = styled(Link)`
-  width: fit-content;
-  padding: 0;
-`;
-
-export const Footer = () => {
-  const { pathname } = useLocation();
-  const despotWorkflowRoutes = [
-    routesEnum.acknowledgementPage,
-    routesEnum.selectClient,
-    routesEnum.generateKeysPage,
-    routesEnum.uploadValidatorPage,
-    routesEnum.connectWalletPage,
-    routesEnum.summaryPage,
-    routesEnum.transactionsPage,
-    routesEnum.congratulationsPage,
-    routesEnum.topUpPage,
-  ];
-
+export const Footer = () => { 
   return (
     <RainbowBackground>
       <FooterStyles>
@@ -83,53 +53,7 @@ export const Footer = () => {
           <Link to={routesEnum.termsOfServicePage}>
             <FormattedMessage defaultMessage="Terms of Service" />
           </Link>
-        </div>
-
-        {!despotWorkflowRoutes.includes(pathname as routesEnum) && (
-          <ButtonContainer className="m-auto">
-            <ButtonLink
-              to={routesEnum.acknowledgementPage}
-              className="cta-button"
-            >
-              <Button
-                rainbow
-                fullWidth
-                width={400}
-                label={
-                  <FormattedMessage
-                    defaultMessage="Become a validator {emoji}"
-                    values={{
-                      emoji: (
-                        <Rhino>
-                          <span role="img" aria-label="rhino">
-                            🦏
-                          </span>
-                        </Rhino>
-                      ),
-                    }}
-                  />
-                }
-              />
-            </ButtonLink>
-          </ButtonContainer>
-        )}
-        {/* <div className="col extra-links">
-          <Heading level={4}>
-            <FormattedMessage defaultMessage="More on staking" />
-          </Heading>
-          <Link to="https://stratisevm.com/en/roadmap/">
-            <FormattedMessage defaultMessage="The Stratis roadmap" />
-          </Link>
-          <Link to={routesEnum.phishingPage}>
-            <FormattedMessage defaultMessage="Avoid phishing" />
-          </Link>
-          <Link to="https://docs.google.com/spreadsheets/d/15tmPOvOgi3wKxJw7KQJKoUe-uonbYR6HF7u83LR5Mj4/edit#gid=842896204">
-            <FormattedMessage defaultMessage="Staking economics" />
-          </Link>
-          <Link to="https://github.com/runtimeverification/deposit-contract-verification/blob/96434de/deposit-contract-verification.pdf">
-            <FormattedMessage defaultMessage="Formal verification report" />
-          </Link>
-        </div> */}
+        </div>        
       </FooterStyles>
     </RainbowBackground>
   );
--- src/pages/GenerateKeys/Instructions.tsx
+++ src/pages/GenerateKeys/Instructions.tsx
@@ -4,8 +4,6 @@ import { keysTool } from './index';
 import { Paper } from '../../components/Paper';
 import { TextSelectionBox } from '../../components/TextSelectionBox';
 import { Option1 } from './Option1';
-import { Option2 } from './Option2';
-import { Option3 } from './Option3';
 import { Heading } from '../../components/Heading';
 
 interface Props {
@@ -37,27 +35,10 @@ export const Instructions = ({
         >
           {formatMessage({ defaultMessage: 'Download CLI app' })}
         </TextSelectionBox>
-        {/* <TextSelectionBox
-          isActive={chosenTool === keysTool.GUI}
-          onClick={() => setChosenTool(keysTool.GUI)}
-          style={{ marginEnd: '20px' }}
-        >
-          {formatMessage({ defaultMessage: 'Download Key Gen GUI app' })}
-        </TextSelectionBox>
-        <TextSelectionBox
-          isActive={chosenTool === keysTool.CLISOURCE}
-          onClick={() => setChosenTool(keysTool.CLISOURCE)}
-        >
-          {formatMessage({ defaultMessage: 'Build from source' })}
-        </TextSelectionBox> */}
       </div>
       <div>
         {chosenTool === keysTool.CLI && (
           <Option1 {...{ validatorCount, withdrawalAddress, os }} />
-        )}
-        {chosenTool === keysTool.GUI && <Option2 {...{ os }} />}
-        {chosenTool === keysTool.CLISOURCE && (
-          <Option3 {...{ validatorCount, withdrawalAddress, os }} />
         )}
       </div>
     </Paper>
